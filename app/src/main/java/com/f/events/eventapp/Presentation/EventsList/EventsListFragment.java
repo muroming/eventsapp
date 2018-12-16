ce84bab62584109f933606c14b0e24bd9abc7b62
Muroming <ulamog345@gmail.com>
2018-12-16 10:57:11 +0300

    Loading EventsList

--- app/src/main/java/com/f/events/eventapp/Presentation/EventsList/EventsListFragment.java
+++ app/src/main/java/com/f/events/eventapp/Presentation/EventsList/EventsListFragment.java
@@ -13,6 +13,12 @@ import android.view.ViewGroup;
 import com.f.events.eventapp.Data.EventDAO;
 import com.f.events.eventapp.R;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,13 +26,21 @@ import java.util.List;
 
 public class EventsListFragment extends Fragment {
 
-    private static RecyclerView recyclerView;
-    private static View view;
+    private static final String ID = "ID";
+
+    private RecyclerView recyclerView;
+    private View view;
     private static List<EventDAO> events;
     private EventsRecyclerAdapter adapter;
+    private FirebaseDatabase mDatabase;
+    private FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
 
-    public static EventsListFragment newInstance(){
-        return new EventsListFragment();
+    public static EventsListFragment newInstance(int id) {
+        Bundle b = new Bundle();
+        b.putInt(ID, id);
+        EventsListFragment fragment = new EventsListFragment();
+        fragment.setArguments(b);
+        return fragment;
     }
 
     @Nullable
@@ -37,6 +51,7 @@ public class EventsListFragment extends Fragment {
         adapter = new EventsRecyclerAdapter(getActivity());
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         recyclerView.setAdapter(adapter);
+        mDatabase = FirebaseDatabase.getInstance();
         return view;
     }
 
@@ -44,10 +59,70 @@ public class EventsListFragment extends Fragment {
     public void onResume() {
         super.onResume();
         setEvents();
-        adapter.addEvents(events);
     }
 
-    public void setEvents(){
-        events = new ArrayList<>();
+    public void setEvents() {
+        String key = mUser.getUid();
+        if (getArguments().getInt(ID, -1) == 0) {
+            mDatabase.getReference("user_created_events").child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    List<String> events = (List<String>) dataSnapshot.getValue();
+                    if (events != null) {
+                        for (String event : events) {
+                            mDatabase.getReference("events").child(event)
+                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            EventDAO event = dataSnapshot.getValue(EventDAO.class);
+                                            adapter.addEvent(event);
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                        }
+                    } else {
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        } else {
+            mDatabase.getReference("users_events").child(key).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    List<String> events = (List<String>) dataSnapshot.getValue();
+                    if (events != null) {
+                        for (String event : events) {
+                            mDatabase.getReference("events").child(event)
+                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                            EventDAO event = dataSnapshot.getValue(EventDAO.class);
+                                            adapter.addEvent(event);
+                                        }
+
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                                        }
+                                    });
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
     }
 }
