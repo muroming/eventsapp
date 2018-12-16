b3364a5c8d552a13c74548c02c0d3464018111d3
Tiny <tanya.hedgehog@gmail.com>
2018-12-16 11:21:25 +0300

    Added Login Design

--- app/src/main/java/com/f/events/eventapp/Presentation/EventsList/EventsFragment.java
+++ app/src/main/java/com/f/events/eventapp/Presentation/EventsList/EventsFragment.java
@@ -7,12 +7,17 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.f.events.eventapp.Data.EventDAO;
 import com.f.events.eventapp.FragmentInteractions;
@@ -22,6 +27,7 @@ import com.google.firebase.auth.internal.FederatedSignInActivity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -33,6 +39,12 @@ public class EventsFragment extends Fragment implements FragmentInteractions.OnB
     List<EventDAO> events;
     @BindView(R.id.pts_switch_events)
     PagerTabStrip pagerTab;
+
+    @BindView(R.id.spinner)
+    Spinner spinner;
+
+    @BindView(R.id.iv_burger)
+    ImageView burger;
 
     public static EventsFragment newInstance() {
         return new EventsFragment();
@@ -48,7 +60,40 @@ public class EventsFragment extends Fragment implements FragmentInteractions.OnB
         pagerAdapter.notifyDataSetChanged();
         events = new ArrayList<>();
         pagerTab.setTabIndicatorColor(getResources().getColor(R.color.white));
+
+        burger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((MainActivity) Objects.requireNonNull(getActivity())).getDrawerLayout().openDrawer(GravityCompat.START);
+            }
+        });
+
+        createSpinner();
+
         return view;
+    }
+
+    public void createSpinner(){
+
+        ArrayAdapter<?> adapter =
+                ArrayAdapter.createFromResource(getContext(), R.array.category, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setSelection(0);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int selectedCategory = spinner.getSelectedItemPosition();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
     }
 
     public class EventsPagerAdapter extends FragmentStatePagerAdapter {
@@ -74,6 +119,7 @@ public class EventsFragment extends Fragment implements FragmentInteractions.OnB
             }
         }
     }
+
 
     @Override
     public void onBackPressed() {
